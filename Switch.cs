9ad4ea7c5d89c3e353dbc3daf1edb5edbf945918
PrakratiToro <prakriti.jain@toro.com>
2026-05-10 22:34:05 +0530

    Create Switch.cs

---
+++ Switch.cs
@@ -0,0 +1,17 @@
+using System;
+class Program{
+	static void Main(string[] args){
+		int number = int.Parse(Console.ReadLine());
+		switch (number){
+			case 1:
+				Console.WriteLine("ONE");
+				break;
+			case 2:
+				Console.WriteLine("TWO");
+				break;
+			case 3:
+				Console.WriteLine("THREE");
+				break;
+		}
+	
+}}
